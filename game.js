5211896ab5d74255e355edf584bac9ff41a7e7f3
Shai Efrati <shaief@gmail.com>
2017-11-05 18:03:41 +0200

    First commit - working version for a single player - 4x4 board

---
+++ game.js
@@ -0,0 +1,100 @@
+Vue.config.devtools = true;
+
+var cardsBack = '❓';
+
+var vm = new Vue({
+      el: '#root',
+      data: function() {
+        return {
+          cards: ['🦀', '🦁', '🦂', '🦃',
+                  '🦄', '🦅', '🦆', '🦇',
+                  '🦈', '🦉', '🦊', '🦋',
+                  '🦌', '🦍', '🦎', '🦏',
+                  '🦐', '🦑', '🦒', '🦓',
+                  '🦔', '🦕', '🦖',
+          ],
+          cardsDeck: [],
+          showCards: {
+            card0: cardsBack,
+            card1: cardsBack,
+            card2: cardsBack,
+            card3: cardsBack,
+            card4: cardsBack,
+            card5: cardsBack,
+            card6: cardsBack,
+            card7: cardsBack,
+            card8: cardsBack,
+            card9: cardsBack,
+            card10: cardsBack,
+            card11: cardsBack,
+            card12: cardsBack,
+            card13: cardsBack,
+            card14: cardsBack,
+            card15: cardsBack,
+          },
+          flippedCardsCounter: 0,
+          flippedCards: [],
+          flippedCardsSymbols: [],
+          twoCards: false,
+          matchedCards: [],
+          userScore: 0,
+        }
+      },
+
+      created: function() {
+        cardsInUse = _.shuffle(this.cards) .slice(0, 8);
+        cardsInUse = [].concat(cardsInUse, cardsInUse);
+        this.cardsDeck = _.shuffle(cardsInUse);
+      },
+      methods: {
+        setupCards: function() {
+          that = this;
+          that.flippedCards.forEach(function(c){
+            if (!that.matchedCards.includes(c)){
+              that.showCards[c] = cardsBack;
+            }
+          })
+          this.flippedCards = [];
+          this.flippedCardsSymbols = [];
+          this.flippedCardsCounter = 0;
+          this.twoCards = false;
+        },
+        exposeCard: function(flippedCard) {
+          var cardNumber = parseInt(flippedCard.split('card')[1]);
+          this.showCards[flippedCard] = this.cardsDeck[cardNumber];
+        },
+        coverCard: function(flippedCard) {
+          this.showCards[flippedCard] = cardsBack;
+        },
+        coverCards: function() {
+          this.setupCards();
+        },
+        flipCard: function(event) {
+          var flippedCard = event.currentTarget.id;
+          if (!this.twoCards & !this.matchedCards.includes(flippedCard)) {
+              if (this.flippedCards[0] != flippedCard) {
+                this.exposeCard(flippedCard);
+                this.flippedCardsCounter += 1;
+                this.flippedCards.push(flippedCard);
+                var cardValue = this.cardsDeck[parseInt(flippedCard.split(
+                  'card')[1])];
+                this.flippedCardsSymbols.push(cardValue);
+                if (this.flippedCardsCounter >= 2) {
+                  this.twoCards = true;
+                  if (this.flippedCardsSymbols[0] == this.flippedCardsSymbols[1]) {
+                    this.matchedCards = this.matchedCards.concat(this.flippedCards);
+                    this.userScore += 1;
+                    this.setupCards();
+                  }
+                }
+              }
+            else {
+              this.coverCard(flippedCard);
+              this.flippedCards = [];
+              this.flippedCardsSymbols = [];
+              this.flippedCardsCounter = 0;
+            }
+            }
+          }
+        },
+      });
